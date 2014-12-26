c0fccf91be7e1dcca982a5396d1ac5b37eebef3c
soarunachalam <somu.arunachalam@gmail.com>
2014-12-26 06:30:35 +0530

    Form validation complete

--- accounting/app.js
+++ accounting/app.js
@@ -18,6 +18,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+customers = [];
 //app.use(bodyparser.json())
 
 app.listen(9000, function(){
@@ -33,17 +34,37 @@ app.get('/customer-information', function(req, res){
 					  "town": "Sivagangai",
 					  "phone":"04575234093"
 					},
-					"primeDetails":{
+					"primeryContactDetails":{
 					  "name": "Somu",
 					  "phone":"8939901380"
 					}
 				};
 		
-	res.render("customerinfo", {"customerInfo":{}});
+	res.render("customerinfo", {"customerInfo":user});
 });
 
 app.post('/customer-information', urlencodedParser, function (req, res) {
 	if (!req.body) return res.sendStatus(400)
-	console.log(req.body);
+	
+	var orgDetailsRecvd = {};
+	var primeryContactDetailsRecvd = {};
+	var customerInfo = {};
+	
+	orgDetailsRecvd["name"] = req.body.orgName;
+	orgDetailsRecvd["addr1"] = req.body.addr1;
+	orgDetailsRecvd["addr2"] = req.body.addr2;
+	orgDetailsRecvd["town"] = req.body.town;
+	orgDetailsRecvd["area"] = req.body.area;
+	orgDetailsRecvd["phone"] = req.body.orgPhone;
+	
+	primeryContactDetailsRecvd["name"] = req.body.primeryContactName;
+	primeryContactDetailsRecvd["phone"] = req.body.primeryContactPhone;
+	
+	customerInfo["orgDetails"] = orgDetailsRecvd;
+	customerInfo["primeryContactDetails"] = primeryContactDetailsRecvd;
+		
+	customers[customers.length] = customerInfo;
+	for(i=0; i<customers.length; i++)
+		console.log(customers[i]);
 	res.sendStatus(404);
 });	
