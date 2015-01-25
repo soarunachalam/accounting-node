2c2b888c3c2192de3956d76d532c5c2e00829f37
soarunachalam <somu.arunachalam@gmail.com>
2015-01-25 15:51:50 +0530

    Modular code

--- accounting/app.js
+++ accounting/app.js
@@ -1,46 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
 var path = require('path');
 var cons = require('consolidate');
 var url = require('url');
 
+var environment = require('./config/environment');
+var routes      = require('./config/routes');
+//var models      = require('./app/models/');
+
 var app = express();
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('views', path.join(__dirname, 'views'));
-app.engine('dust', cons.dust);
-app.set('view engine', 'dust');
-
-var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.use(bodyparser.json())
+environment(app);
+routes(app);
 
 app.listen(9000, function(){
 	console.log("Server started");
 	});
 
-app.get('/customer-information', function(req, res){
-	var user = {				
-					"orgDetails": {
-					  "name": "Somu",
-					  "addr1": "9 Harrington Road",
-					  "addr2": "Ram Nagar",
-					  "town": "Sivagangai",
-					  "phone":"04575234093"
-					},
-					"primeDetails":{
-					  "name": "Somu",
-					  "phone":"8939901380"
-					}
-				};
-		
-	res.render("customerinfo", {"customerInfo":{}});
-});
 
 app.post('/customer-information', urlencodedParser, function (req, res) {
 	if (!req.body) return res.sendStatus(400)
